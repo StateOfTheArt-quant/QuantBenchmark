b88ab2931bc3ac1dd17a10e829e75bf32ce09828
yoyo182487329 <yoyo182487329@163.com>
2019-11-15 13:36:24 +0800

    original version

--- quant_benchmark/__main__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 15 13:30:09 2019
-
-@author: luyao
-"""
-
--- quant_benchmark/interface.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 15 13:30:38 2019
-
-@author: luyao
-"""
-
